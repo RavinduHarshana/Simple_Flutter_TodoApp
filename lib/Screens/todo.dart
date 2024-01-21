2fc54635312f58fb7ae9e15e4e519afa1c22495b
RavinduHarshana <ravinduharshana2001@gmail.com>
2024-01-21 11:01:36 +0530

    create todo page

---
+++ lib/Screens/todo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class todoui extends StatelessWidget {
+  const todoui({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.amber,
+    );
+  }
+}
